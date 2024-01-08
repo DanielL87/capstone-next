6779b4cbe88b6004d534bca4447122f89c641cec
Daniel Lira <daniel.liras87@gmail.com>
2024-01-08 15:10:14 -0500

    removes more boiler plate code, and add env file

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <main></main>;
+  return <div className={styles.container}>Test</div>;
 }
