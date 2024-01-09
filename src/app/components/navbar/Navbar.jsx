ad9953e7c91a00c4203e6c8c8dcee4b964ecd56d
IntermediaDesigns <intermediadesignsllc@gmail.com>
2024-01-09 15:24:25 -0500

    Added folders, navbar, prisma, fetchuser

---
+++ src/app/components/navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default async function Navbar() {
+  return (
+    <div>navbar</div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,5 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.container}>Test</div>;
+  return <div>Test</div>;
 }
