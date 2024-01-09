400d0f5f141004cee9c8dca62c78687b0dbdf4cb
Daniel Lira <daniel.liras87@gmail.com>
2024-01-09 16:15:48 -0500

    reintialize git

---
+++ src/app/lib/fetchUser.js
@@ -0,0 +1,26 @@
+import { prisma } from './prisma.js';
+import { cookies } from 'next/headers';
+import jwt from 'jsonwebtoken';
+
+export async function fetchUser() {
+  try {
+    const cookieStore = cookies();
+    const userCookie = cookieStore.get('token');
+
+    if (!userCookie) {
+      return { user: {}, token: null };
+    }
+    const decodedToken = jwt.verify(userCookie.value, process.env.JWT_SECRET);
+
+    const { userId } = decodedToken;
+
+    const user = await prisma.user.findFirst({ where: { id: userId } });
+
+    delete user.password;
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    return { user: {}, token: null };
+  }
+}
