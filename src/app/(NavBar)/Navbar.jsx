c4221894d7faacd450ffbe6b3f767a1d3e12676c
Daniel Lira <daniel.liras87@gmail.com>
2024-01-10 14:37:24 -0500

    adds logout component and conditional rendering on navbar

--- src/app/(NavBar)/Navbar.jsx
+++ src/app/(NavBar)/Navbar.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import Link from 'next/link';
+import Link from "next/link";
+import Logout from "../components/Logout.jsx";
+import { fetchUser } from "../lib/fetchUser.js";
 
 export default async function Navbar() {
+  const user = await fetchUser();
+  console.log(user);
+
   return (
     <div>
       <Link href={"/"}> Home</Link>
-      <Link className="sub" href={"/login"}>
-        Login
-      </Link>
-      <Link href={"/register"}>Sign Up</Link>
+      {!user.id ? (
+        <div>
+          <Link className="sub" href={"/login"}>
+            Login
+          </Link>
+          <Link href={"/register"}>Sign Up</Link>
+        </div>
+      ) : (
+        <div>
+          <div>Welcome {user.username}</div>
+          <Logout />
+        </div>
+      )}
     </div>
   );
 }
