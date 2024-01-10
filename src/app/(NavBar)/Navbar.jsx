f02780ed136f83bfd146f35165dd5ba248ec4131
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2024-01-10 14:39:45 -0500

    .

--- src/app/(NavBar)/Navbar.jsx
+++ src/app/(NavBar)/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import styles from '../page.module.css';
 import Link from 'next/link';
 
 export default async function Navbar() {
   return (
-    <div>
+    <div className={styles.navBarContainer}>
       <Link href={"/"}> Home</Link>
-      <Link className="sub" href={"/login"}>
+      <Link className={styles.login} href={"/login"}>
         Login
       </Link>
       <Link href={"/register"}>Sign Up</Link>
