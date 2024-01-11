ad3a9a3d3f15b9e96374056d5164521c021c8054
Moreen Nampeera <nampeeramoreenp@gmail.com>
2024-01-12 00:00:48 +0400

    added pokemon css

---
+++ src/app/components/PokemonDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styles from "../page.module.css";
+
+export default function PokemonDetails({ pokemon }) {
+  return (
+    <div className={styles.pokemonContainer}>
+      <div key={pokemon.id}>
+        <p> {pokemon.capitalizedName} </p> <p>ID:{pokemon.id}</p>{" "}
+        <p>Type: {pokemon.type}</p>
+        <img src={pokemon.sprite} alt={`${pokemon.name} sprite`} />
+      </div>
+    </div>
+  );
+}
