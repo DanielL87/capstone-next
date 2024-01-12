a19510e353ddc1a6bcbe59b8bd4dd0c68f50ddbb
Daniel Lira <daniel.liras87@gmail.com>
2024-01-11 21:42:26 -0500

    adds a gradient to card background colors

--- src/app/components/PokemonDetails.jsx
+++ src/app/components/PokemonDetails.jsx
@@ -4,10 +4,17 @@ import pokeColor from "../lib/pokeColor.js";
 import styles from "../page.module.css";
 
 export default function PokemonDetails({ pokemon }) {
+  const gradient = pokeColor[pokemon.type.toLowerCase()] || {
+    start: "#ffffff",
+    end: "#ffffff",
+  };
+
+  const gradientBackground = `linear-gradient(45deg, ${gradient.start}, ${gradient.end})`;
+
   return (
     <div
       className={styles.pokemonContainer}
-      style={{ backgroundColor: pokeColor[pokemon.type] }}
+      style={{ background: gradientBackground }}
     >
       <div key={pokemon.pokedexId}>
         <p> {pokemon.capitalizedName} </p> <p>ID:{pokemon.pokedexId}</p>{" "}
