91571388a6decc19dcf607cbe240540b05184c0f
Daniel Lira <daniel.liras87@gmail.com>
2024-01-17 04:52:33 -0500

    adds more details to single pokemon page

--- src/app/(navigation)/pet/[petId]/page.jsx
+++ src/app/(navigation)/pet/[petId]/page.jsx
@@ -1,10 +1,15 @@
-"use client";
 import React from "react";
-import { useParams } from "next/navigation.js";
-import SinglePetPage from "@/app/components/SinglePetPage.jsx";
+import { prisma } from "@/app/lib/prisma.js";
+import SinglePetInfo from "@/app/components/SinglePetPage.jsx";
 
-export default function PetPage() {
-  const { petId } = useParams();
+export default async function PetPage({ params }) {
+  const petId = params.petId;
+  const pet = await prisma.pet.findFirst({ where: { id: petId } });
 
-  return <SinglePetPage petId={petId} />;
+  const pokemon = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pet.pokedexId}`
+  );
+  const pokemonData = await pokemon.json();
+
+  return <SinglePetInfo pet={pet} pokemonData={pokemonData} />;
 }
