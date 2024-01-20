f98d67fd31b41d40750f3eb777bbee030b4502c2
Daniel Lira <daniel.liras87@gmail.com>
2024-01-20 15:15:57 -0500

    sets a 151 limit to pokedex

--- src/app/(navigation)/pokedex/page.jsx
+++ src/app/(navigation)/pokedex/page.jsx
@@ -1,7 +1,7 @@
-'use client';
-import React, { useState } from 'react';
-import PokemonList from '../../components/PokemonList.jsx';
-import styles from '../../page.module.css';
+"use client";
+import React, { useState } from "react";
+import PokemonList from "../../components/PokemonList.jsx";
+import styles from "../../page.module.css";
 
 export default function Pokedex() {
   const [endId, setEndId] = useState(10);
@@ -9,17 +9,28 @@ export default function Pokedex() {
 
   async function handleShowMore() {
     setIsLoading(true);
-    await new Promise(resolve => setTimeout(resolve, 2000)); // Simulate a delay
-    setEndId(prevEndId => prevEndId + 10);
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay
+    if (endId + 10 <= 151) {
+      setEndId((prevEndId) => prevEndId + 10);
+    } else {
+      setEndId(151);
+    }
+
     setIsLoading(false);
   }
 
   return (
     <div className={styles.pokedexCardMainContainer}>
       <PokemonList startId={1} endId={endId} />
-      <button className={styles.loginBtn} onClick={handleShowMore} disabled={isLoading}>
-        {isLoading ? 'Loading...' : 'Show More'}
-      </button>
+      {endId < 151 ? (
+        <button
+          className={styles.loginBtn}
+          onClick={handleShowMore}
+          disabled={isLoading}
+        >
+          {isLoading ? "Loading..." : "Show More"}
+        </button>
+      ) : null}
     </div>
   );
 }
