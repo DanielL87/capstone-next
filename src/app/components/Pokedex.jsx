aa1dfdca99416fd79ad061aa4dbe529e90816fc4
Moreen Nampeera <nampeeramoreenp@gmail.com>
2024-01-28 22:46:22 +0400

    added the showless functionality

--- src/app/components/Pokedex.jsx
+++ src/app/components/Pokedex.jsx
@@ -1,9 +1,11 @@
 "use client";
 import React, { useState } from "react";
-import PokemonList from "./PokemonList.jsx";
+
 import styles from "../page.module.css";
+import PokemonList from "./PokemonList.jsx";
 
 export default function Pokedex({ collection }) {
+  const [startId, setStartId] = useState(1);
   const [endId, setEndId] = useState(10);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -12,16 +14,31 @@ export default function Pokedex({ collection }) {
     await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay
     if (endId + 10 <= 151) {
       setEndId((prevEndId) => prevEndId + 10);
+      setStartId((prevStartId) => prevStartId + 10);
     } else {
       setEndId(151);
+      setStartId(141);
     }
 
     setIsLoading(false);
   }
 
+  async function handleShowLess() {
+    setIsLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    if (startId - 10 >= 1) {
+      setEndId((prevEndId) => prevEndId - 10);
+      setStartId((prevStartId) => prevStartId - 10);
+    } else {
+      setStartId(1);
+      setEndId(10);
+    }
+    setIsLoading(false);
+  }
+
   return (
     <div className={styles.pokedexCardMainContainer}>
-      <PokemonList startId={1} endId={endId} collection={collection} />
+      <PokemonList startId={startId} endId={endId} collection={collection} />
       {endId < 151 ? (
         <button
           className={styles.loginBtn}
@@ -29,6 +46,16 @@ export default function Pokedex({ collection }) {
           disabled={isLoading}
         >
           {isLoading ? "Loading..." : "Show More"}
+        </button>
+      ) : null}
+      {/* handle showless */}
+      {endId > 10 ? (
+        <button
+          className={styles.loginBtn}
+          onClick={handleShowLess}
+          disabled={isLoading}
+        >
+          {isLoading ? "Loading..." : "Show Less"}
         </button>
       ) : null}
     </div>
