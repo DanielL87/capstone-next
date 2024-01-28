589ea269dcdd54342369ac18f814c5119958ce04
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2024-01-28 13:16:29 -0500

    Update CreateTask component and page.module.css

--- src/app/components/CreateTask.jsx
+++ src/app/components/CreateTask.jsx
@@ -1,51 +1,57 @@
-"use client";
-import React, { useState } from "react";
-import styles from "../page.module.css";
-import { useRouter } from "next/navigation.js";
+'use client';
+import React, { useState } from 'react';
+import styles from '../page.module.css';
+import { useRouter } from 'next/navigation.js';
 
 export default function CreateTask({ user, pet }) {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [worth, setWorth] = useState(1);
-  const [category, setCategory] = useState("");
-  const [message, setMessage] = useState("");
+  const [category, setCategory] = useState('');
+  const [message, setMessage] = useState('');
   const router = useRouter();
   const [isLoggedIn, setIsLoggedIn] = useState(user.id);
   const [submitClicked, setSubmitClicked] = useState(false);
-  const [taskName, setTaskName] = useState("");
+  
   const [error, setError] = useState(null);
+  const [dueDate, setDueDate] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    const task = {
+      category,
+      name,
+      dueDate,
+    };
+
     setSubmitClicked(true);
 
     if (!isLoggedIn) {
-      setError("You must be logged in to submit.");
+      setError('You must be logged in to submit.');
       return;
     }
 
-    if (!taskName) {
-      setError("Fields must be filled before submitting.");
-      return;
-    }
+    
 
     try {
-      const response = await fetch("/api/tasks", {
-        method: "POST",
+      const response = await fetch('/api/tasks', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          name: taskName,
-          category: category,
+          name: task.name,
+          category: task.category,
           worth: worth,
           petId: pet.id,
+          dueDate: task.dueDate,
         }),
       });
       const data = await response.json();
       if (data.error) {
         setError(data.error);
       } else {
-        setTaskName("");
+        setTaskName('');
         router.refresh();
       }
     } catch (error) {
@@ -56,33 +62,41 @@ export default function CreateTask({ user, pet }) {
   return (
     <>
       <div className={styles.taskFormMainContainer}>
-        <form id="taskForm" className={styles.taskForm} onSubmit={handleSubmit}>
+        <form id='taskForm' className={styles.taskForm} onSubmit={handleSubmit}>
           <p className={styles.formTitle}>Create a Task</p>
           <div className={styles.taskCategoryContainer}>
             <label className={styles.categoryTitle}>Task Category</label>
             <input
               className={styles.categoryInput}
-              type="text"
+              type='text'
               value={category}
               onChange={(e) => setCategory(e.target.value)}
-              placeholder="Enter a Category.."
+              placeholder='Enter a Category..'
+              required
+            />
+            <label className={styles.categoryTitle}>Due Date</label>
+            <input
+              className={styles.categoryInput}
+              type='date'
+              value={dueDate}
+              onChange={(e) => setDueDate(e.target.value)}
               required
             />
           </div>
           <input
             className={styles.taskInput}
-            type="text"
+            type='text'
             value={name}
             onChange={(e) => setName(e.target.value)}
-            placeholder="Enter a Task.."
+            placeholder='Enter a Task..'
             required
           />
         </form>
-        <button form="taskForm" className={styles.registerBtn} type="submit">
+        <button form='taskForm' className={styles.registerBtn} type='submit'>
           Submit
         </button>
         {error && <p className={styles.errorText}>{error}</p>}
-        {message && <p className={styles.successText}>{message}</p>}{" "}
+        {message && <p className={styles.successText}>{message}</p>}{' '}
       </div>
     </>
   );
