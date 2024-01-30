776a69d03e0c822a7b91c5d6b4f5c98ec20d8510
Daniel Lira <daniel.liras87@gmail.com>
2024-01-30 14:52:41 -0500

    adds condition to check tasks in back end

--- src/app/components/GenerateBonusTask.jsx
+++ src/app/components/GenerateBonusTask.jsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 
 export default function GenerateBonusTask({ pet }) {
   const router = useRouter();
+
   function getRandomTask() {
     const randomIndex = Math.floor(Math.random() * bonustasks.length);
     return bonustasks[randomIndex];
@@ -16,29 +17,32 @@ export default function GenerateBonusTask({ pet }) {
     return info.pet.task;
   }
 
-  async function GenerateBonusTask() {
-    const petTasks = await fetchPetData();
+  async function GenerateTask() {
+    if (Math.random() < 0.1) {
+      const petTasks = await fetchPetData();
 
-    const filteredTasks = petTasks.filter(
-      (task) => task.isBonus && !task.isCompleted
-    );
+      const filteredTasks = petTasks.filter(
+        (task) => task.isBonus && !task.isCompleted
+      );
 
-    if (filteredTasks.length > 3) {
-      if (pet.hearts === 0) {
-        console.log("Pet ran away");
-      } else if (pet.hearts > 0) {
-        const response = await fetch(`/api/pets/`, {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            petId: pet.id,
-          }),
-        });
-        const info = await response.json();
+      if (filteredTasks.length >= 3) {
+        if (pet.hearts === 0) {
+          // console.log("Pet ran away");
+        } else if (pet.hearts > 0) {
+          const response = await fetch(`/api/pets/`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              petId: pet.id,
+            }),
+          });
+          const info = await response.json();
+
+          router.refresh();
+        }
       }
-    }
 
-    if (filteredTasks.length < 3) {
+      // if (filteredTasks.length < 3) {
       let randomTask = getRandomTask();
       const dueDate = new Date();
       dueDate.setDate(dueDate.getDate() + 1);
@@ -57,14 +61,17 @@ export default function GenerateBonusTask({ pet }) {
         }),
       });
       const info = await response.json();
+
+      console.log(info);
+      router.refresh();
     }
   }
 
   // useEffect to run GenerateTask every second
   useEffect(() => {
     const intervalId = setInterval(() => {
-      GenerateBonusTask();
-    }, 60000);
+      GenerateTask();
+    }, 1000);
 
     return () => clearInterval(intervalId);
   }, []);
